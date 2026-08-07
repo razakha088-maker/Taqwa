9e2801fd6e9bdfc9e88e230c11039539e133657e
razakha088-maker <razakha088@gmail.com>
2026-08-07 21:47:54 +0600

    Import HomeScreen from theme package

--- app/src/main/java/com/taqwa/guard/MainActivity.kt
+++ app/src/main/java/com/taqwa/guard/MainActivity.kt
@@ -3,8 +3,8 @@ package com.taqwa.guard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.taqwa.guard.ui.HomeScreen
 import com.taqwa.guard.ui.theme.GuardTheme
+import com.taqwa.guard.ui.theme.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
