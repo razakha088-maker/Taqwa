4c947103778dc9184f18e42bbb851f3ba175908b
razakha088-maker <razakha088@gmail.com>
2026-08-09 11:11:09 +0600

    Fix accessibility service Kotlin

--- app/src/main/java/com/taqwa/guard/accessibility/TaqwaAccessibilityService.kt
+++ app/src/main/java/com/taqwa/guard/accessibility/TaqwaAccessibilityService.kt
@@ -21,18 +21,3 @@ class TaqwaAccessibilityService : AccessibilityService() {
         Log.d("TaqwaProtection", "Service interrupted")
     }
 }
-package com.taqwa.guard.accessibility
-
-import android.accessibilityservice.AccessibilityService
-import android.view.accessibility.AccessibilityEvent
-
-class TaqwaAccessibilityService : AccessibilityService() {
-
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        // Screen monitoring will be added in the next step.
-    }
-
-    override fun onInterrupt() {
-        // Service interrupted by the system.
-    }
-}
